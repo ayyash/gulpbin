6918b20d63acf78ca860f4f1a0a9364309b9e203
Ayyash <abdul_ayyash@yahoo.com>
2022-09-06 14:44:16 +0300

    removed test

--- client/index.js
+++ client/index.js
@@ -1,21 +1,55 @@
 /****
  * This is an example folder to be used in client that uses the gulpBin commands.
  ***/
-
-const config = require('./config.json');
-// setup config
-
-
-// TODO: a bit more helpful content
-
-// or bring them all in
-const gulpBin = require('sekrab-gulpbin')(config);
-
-// expose them all
-Object.keys(gulpBin).forEach(i => {
-    Object.keys(gulpBin[i]).forEach( x =>  {
-        exports[x] = gulpBin[i][x];
-
-    });
-});
-
+ const config = require('./config.json');
+ const gulpBin = require('sekrab-gulpbin');
+ 
+ const localConfig = { ...gulpBin.defaultConfig, ...config };
+ 
+ // assets, css and mirroring
+ const allAssets = gulpBin.assets(localConfig);
+ // generate css from less
+ exports.rawless = allAssets.rawless;
+ // also clean, and minify into public (no critical)
+ exports.buildcss = allAssets.buildcss;
+ // generate critical seperated css files
+ exports.critical = allAssets.critical;
+ // watch and rawless
+ exports.watch = allAssets.watch;
+ 
+ // gererate icons from ico-moon generated files and less
+ exports.icons = gulpBin.icons(localConfig);
+ 
+ // extract translation terms into src/locale/prefix-lang.js files
+ exports.extract = gulpBin.extract(localConfig);
+ 
+ // genreate index.lang.html into host client, from placeholder
+ exports.locales = gulpBin.locales(localConfig);
+ 
+ // example CUSTOM task
+//  exports.writesurge = gulpBin.locales({
+// 	 ...localConfig,
+// 	 locales: {
+// 		 source: '../surge/client/placeholder.html',
+// 		 destination: '../surge/client/',
+// 		 fileName: '200.html',
+// 		 withFolders: true,
+// 		 isUrlBased: false
+// 	 }
+//  });
+ 
+ // ng generators
+ const ng = gulpBin.ng(localConfig);
+ 
+ exports.injectServices = ng.injectServices;
+ exports.injectLib = ng.injectLibModule;
+ exports.injectModels = ng.injectModels;
+ exports.inject = ng.injectAll;
+ exports.routemodule = ng.createRouteModule;
+ exports.component = ng.createComponent;
+ exports.pipe = ng.createPipe;
+ exports.directive = ng.createDirective;
+ exports.model = ng.createModel;
+ exports.service = ng.createService;
+ exports.fullService = ng.createFullService;
+ 
